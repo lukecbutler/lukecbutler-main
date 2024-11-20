c1bf1256bbffb7dc51ab3937b7c8ae9a08be2366
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-20 05:43:03 -0500

    update site

--- app.py
+++ app.py
@@ -8,61 +8,62 @@ contacts = []
 
 @app.route("/")
 def home():
-    return render_template("/home.html")
+    return render_template("homepage.html")
 
 
+####################################################################################
+"""
+Contact Book Routes:
+"""
+####################################################################################
+#contact book homepage
+@app.route("/contact/home")
+def contactHome():
+    return render_template("home_contact.html")
 
-
-@app.route("/show_contacts")
+@app.route("/contact/show")
 def showContacts():
-    return render_template("/show_contacts.html", contacts = contacts)
+    return render_template("show_contacts.html", contacts = contacts)
 
-
-
-
-@app.route("/add_contact")
+@app.route("/contact/add", methods=["GET", "POST"])
 def add_contact():
+
+    # if statement occurs when form submission is hit
+    if request.method =="POST":
+        # Handle form submission
+        name = request.form.get("contact-name")
+        number = request.form.get("contact-number")
+
+        # Handle missing input
+        if not name or not number:
+            return render_template("add_contact.html", error = "Please fill out both fields.")
+
+        contacts.append([name, number])
+        return render_template("home_contact.html")
+    
     return render_template("add_contact.html")
 
-@app.route("/handleAddContact", methods=["POST"])
-def handleAddContact(): #commit test2
-
-    name = request.form.get("contact-name")
-    number = request.form.get("contact-number")
-    contacts.append([name,number])
-
-    return render_template("/home.html")
-
-
-
-
-@app.route("/delete_contact")
+@app.route("/contact/delete", methods=["GET", "POST"])
 def delete_contact():
 
-    return render_template("/delete_contact.html", contacts = contacts)
+    if request.method == "POST":
+        contact_number_input = request.form.get("contact-number")
+        if not contact_number_input.isdigit():
+            return render_template("delete_contact.html", contacts=contacts, error="Enter a number bucko")
 
-@app.route("/handleDeleteContact", methods=["POST"])
-def handleDeleteContact():
+        contact_number = int(contact_number_input)
 
-    contact_number_input = request.form.get("contact-number")
-    if not contact_number_input.isdigit():
-        return render_template("/delete_contact.html", contacts=contacts, error="Please enter a valid number")
-
-    contact_number = int(contact_number_input)
-
-    # Checks if contacts list is empty - if empty prompt user no contact to delete
-    if not contacts:
-        return render_template("/delete_contact.html", contacts=contacts, error="No contacts you silly goose.")
+        # Checks if contacts list is empty - if empty prompt user no contact to delete
+        if not contacts:
+            return render_template("delete_contact.html", contacts=contacts, error="No contacts you silly goose.")
     
-    # Check if contact_number is within range - if contact number is not within range return page until user enters appropriate number
-    if contact_number < 1 or contact_number > len(contacts):
-        return render_template("/delete_contact.html", contacts=contacts, error="Add a number first!!")
+        # Check if contact_number is within range - if contact number is not within range return page until user enters appropriate number
+        if contact_number < 1 or contact_number > len(contacts):
+            return render_template("delete_contact.html", contacts=contacts, error="Add a contact first!!")
 
+        contacts.pop(contact_number-1)
 
-    contacts.pop(contact_number-1)
-
-    return render_template("/home.html")
- 
+    return render_template("delete_contact.html", contacts = contacts)
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host="0.0.0.0")
